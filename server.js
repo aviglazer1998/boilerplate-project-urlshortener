6106acdd4eb35f6e651e8d278539057fe4d2c863
Aglazer <51724278+aviglazer1998@users.noreply.github.com>
2021-12-08 14:40:09 -0500

    added to git

--- server.js
+++ server.js
@@ -1,24 +1,64 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+const dns = require("dns");
 
-// Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+	res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+let count = 0;
+
+function getKeyByValue(object, value) {
+	return Object.keys(object).find((key) => object[key] === value);
+}
+
+let obj = {};
+//https://www.geeksforgeeks.org/check-if-an-url-is-valid-or-not-using-regular-expression/
+app.post("/api/:url", (req, res) => {
+	let regex =
+		"((http|https)://)(www.)?" + "[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]" + "{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)";
+	let url = regex.exec(req.body.url);
+	if (url === null) res.json({ error: "Invalid URL" });
+	else {
+		let result = getKeyByValue(obj, url.input);
+		if (result === undefined) {
+			count++;
+			obj[count] = req.body.url;
+			res.json({
+				original_url: req.body.url,
+				short_url: count,
+			});
+		} else {
+			res.json({
+				original_url: req.body.url,
+				short_url: result,
+			});
+		}
+	}
+});
+
+app.get("/api/shorturl/:shorturl", (req, res) => {
+	let shorturl = req.params.shorturl;
+	let result = obj[shorturl];
+	if (result === undefined) res.json({ error: "Invalid URL" });
+	else res.redirect(obj[shorturl]);
+});
+
+app.listen(port, function () {
+	console.log(`Listening on port ${port}`);
 });
